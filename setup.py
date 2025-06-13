f9aefb1de0e80302140f4e403a904b58098923f7
Jorge Santiago Rodríguez Peñalosa <jspenalosa@icloud.com>
2025-06-13 19:18:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     name='mutualinfo',
     version='0.1.0',
     description='Librería para estimar la información mutua y cuantificar su incertidumbre',
-    author='Tu Nombre',
-    author_email='tu.email@example.com',
-    url='https://github.com/tu_usuario/mutualinfo',  # Actualiza con tu URL real
+    author='Jorge Santiago Rodríguez Peñalosa',
+    author_email='jspenalosa@icloud.com',
+    url='https://github.com/JSRP00/mutualinfo',  # Actualiza con tu URL real
     packages=find_packages(),
     install_requires=[
         'numpy',
