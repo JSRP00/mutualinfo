0d931589ce8c70e033941cf628b679238bfa103c
Jorge Santiago Rodríguez Peñalosa <jspenalosa@icloud.com>
2025-06-13 19:07:21 +0200

    Update __init__.py

--- mutualinfo/estimators/__init__.py
+++ mutualinfo/estimators/__init__.py
@@ -1,8 +1,8 @@
 # mutualinfo/estimators/__init__.py
 
 from .kraskov import estimate_mi as estimate_mi_kraskov
-from .kde import estimate_mi_kde
-from .histograms import estimate_mi_histogram
+from .kde import estimate_mi as estimate_mi_kde
+from .histograms import estimate_mi as estimate_mi_histogram
 
 __all__ = [
     "estimate_mi_kraskov",
