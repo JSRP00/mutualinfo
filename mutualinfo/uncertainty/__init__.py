6aa2f31944bdfa2fc57f879200b1e2e988bd9678
Jorge Santiago Rodríguez Peñalosa <jspenalosa@icloud.com>
2025-06-25 07:41:48 +0200

    Update __init__.py

--- mutualinfo/uncertainty/__init__.py
+++ mutualinfo/uncertainty/__init__.py
@@ -1,9 +1,9 @@
 # mutualinfo/uncertainty/__init__.py
 
 from .bootstrap import bootstrap_ci
-from .conformal import conformal_ci
+from .conformal import split_conformal_regression
 
 __all__ = [
     "bootstrap_ci",
-    "conformal_ci"
+    "split_conformal_regression"
 ]
