986048298690316bbc250783ebac136b07be7293
Jorge Santiago Rodríguez Peñalosa <jspenalosa@icloud.com>
2025-06-25 08:32:05 +0200

    Update conformal.py

--- mutualinfo/uncertainty/conformal.py
+++ mutualinfo/uncertainty/conformal.py
@@ -2,7 +2,6 @@ from mapie.regression import SplitConformalRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 import numpy as np
-import copy
 
 def split_conformal_regression(X, y, alpha=0.1, test_size=0.2, cal_size=0.2, random_state=42):
     """
