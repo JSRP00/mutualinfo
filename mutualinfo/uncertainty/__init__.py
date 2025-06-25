52da6d7f68402d3550a98e41689427b431283fde
Jorge Santiago Rodríguez Peñalosa <jspenalosa@icloud.com>
2025-06-25 12:34:30 +0200

    Update __init__.py

--- mutualinfo/uncertainty/__init__.py
+++ mutualinfo/uncertainty/__init__.py
@@ -1,9 +1,10 @@
 # mutualinfo/uncertainty/__init__.py
 
 from .bootstrap import bootstrap_ci
-from .conformal import split_conformal_prediction
+from .conformal import split_conformal_prediction, encode_prediction_sets
 
 __all__ = [
     "bootstrap_ci",
     "split_conformal_prediction"
+    "encode_prediction_sets"
 ]
