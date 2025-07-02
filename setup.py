fe7555d307c2b880d9158490303d25deb7a15d16
Jorge Santiago Rodríguez Peñalosa <jspenalosa@icloud.com>
2025-07-02 13:01:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@ setup(
     description='Librería para estimar la información mutua y cuantificar su incertidumbre',
     author='Jorge Santiago Rodríguez Peñalosa',
     author_email='jspenalosa@icloud.com',
-    url='https://github.com/JSRP00/mutualinfo',  # Actualiza con tu URL real
+    url='https://github.com/JSRP00/mutualinfo', 
     packages=find_packages(),
     install_requires=[
         'numpy',
         'scipy',
         'scikit-learn',
         'matplotlib',
+        'mapie',
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
