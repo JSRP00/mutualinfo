dc1a7a16a5b22602ab92e596d378aacd63fbfc74
Jorge Santiago Rodríguez Peñalosa <jspenalosa@icloud.com>
2025-07-08 07:36:23 +0200

    Update __init__.py
    estimate_mi_from_conformal_prediction_sets

--- mutualinfo/uncertainty/__init__.py
+++ mutualinfo/uncertainty/__init__.py
@@ -1,11 +1,12 @@
 # mutualinfo/uncertainty/__init__.py
 
-from .conformal import split_conformal_prediction, encode_prediction_sets, predict_confidence_regions
+from .conformal import split_conformal_prediction, encode_prediction_sets, predict_confidence_regions, estimate_mi_from_conformal_prediction_sets
 from .bootstrap import bootstrap_ci
 
 __all__ = [
     "split_conformal_prediction",
     "encode_prediction_sets",
     "predict_confidence_regions",
+    "estimate_mi_from_conformal_prediction_sets",
     "bootstrap_ci"
 ]
