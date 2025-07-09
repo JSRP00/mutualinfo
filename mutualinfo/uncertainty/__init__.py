f04b4a6f2bc10a880bd65d3c6bcf9e6862d406ac
Jorge Santiago Rodríguez Peñalosa <jspenalosa@icloud.com>
2025-07-09 10:18:04 +0200

    Update __init__.py

--- mutualinfo/uncertainty/__init__.py
+++ mutualinfo/uncertainty/__init__.py
@@ -1,16 +1,15 @@
 # mutualinfo/uncertainty/__init__.py
 
-from .conformal import split_conformal_prediction, encode_prediction_sets, predict_confidence_regions, estimate_mi_from_conformal_prediction_sets, estimate_mi_with_uncertainty, estimate_mi_kraskov_conformal, regression_coverage_score_manual, estimate_mi_cp_radius 
 from .bootstrap import bootstrap_ci
-
-__all__ = [
-    "split_conformal_prediction",
-    "encode_prediction_sets",
-    "predict_confidence_regions",
-    "estimate_mi_from_conformal_prediction_sets",
-    "estimate_mi_with_uncertainty",
-    "estimate_mi_kraskov_conformal",
-    "regression_coverage_score_manual",
-    "estimate_mi_cp_radius",
-    "bootstrap_ci"
-]
+from .kraskov_cp import (
+    estimate_mi_kraskov_conformal,
+    regression_coverage_score_manual
+)
+from .conformal import (
+    split_conformal_prediction,
+    encode_prediction_sets,
+    predict_confidence_regions,
+    estimate_mi_from_conformal_prediction_sets,
+    estimate_mi_with_uncertainty,
+    estimate_mi_cp_radius
+)
